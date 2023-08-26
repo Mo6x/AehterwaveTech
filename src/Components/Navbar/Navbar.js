0dc887fed5cc31ccb285f30c72fcdb4eba5f9b09
mo6x <christmoses1999@gmail.com>
2023-08-26 20:24:23 +0100

    still working on this project

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,18 +7,8 @@ import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
-
-  
   return (
     <div className='Navbar-container'>
         <div className='navbar-section'>
@@ -26,9 +16,8 @@ const Navbar = () => {
               <img src={logo} alt='Company-logo' />
               <h1 className='appgenius'>AETHERWAVE  TECHNOLOGIES</h1>
             </div>
-            <div className='Navbar-lists'> 
+            <div className= {isMobile ? 'avater-icon' : 'Navbar-lists'} onClick={() => setIsMobile(false)}> 
               <div className='list-link'>
-                <div isModalOpen={isModalOpen} onClose={closeModal} className='close-button'>X</div>
                <ul>
                 <li> <Link  className="link" to="/">Home</Link> </li>
                 <li> <Link  className="link" to="/About">About-Us</Link> </li>
@@ -37,10 +26,11 @@ const Navbar = () => {
                 <li> <Link  className="link" to="/Contact">Contact-Us</Link></li>
                </ul>
                </div>
-              <div className='avater-img' onClick={openModal}>
-               <img src={ Avater} alt='avatar images' />
+              <div className='avater-icon' onClick={() => setIsMobile(!isMobile)}>
+               {isMobile ? (<img src={ Avater} alt='avatar images' className='icon-img'/>) : (
+                <div className='close-button'>X</div>)}
               </div>
-             </div>
+            </div>
         </div>
     </div>
   )
