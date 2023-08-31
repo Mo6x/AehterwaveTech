9bec3af5a8585e55e01202ac59f2d594ef755c20
mo6x <christmoses1999@gmail.com>
2023-08-31 20:11:16 +0100

    still working on this project

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,39 +1,41 @@
 import React, { useState } from 'react';
 import '../Navbar/Navbar.css';
 import logo from '../../assert/logo.png';
-import Avater from '../../assert/noavatar.jpg';
-import { Link } from "react-router-dom";
 
 
 
 const Navbar = () => {
-  const [isMobile, setIsMobile] = useState(false);
+  
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   return (
-    <div className='Navbar-container'>
-        <div className='navbar-section'>
-            <div className='navbe-img'>
-              <img src={logo} alt='Company-logo' />
-              <h1 className='appgenius'>AETHERWAVE  TECHNOLOGIES</h1>
-            </div>
-            <div className= {isMobile ? 'avater-icon' : 'Navbar-lists'} onClick={() => setIsMobile(false)}> 
-              <div className='list-link'>
-               <ul>
-                <li> <Link  className="link" to="/">Home</Link> </li>
-                <li> <Link  className="link" to="/About">About-Us</Link> </li>
-                <li> <Link  className="link" to="/Services">Services</Link></li>
-                <li> <Link  className="link" to="/Impact">Impact</Link> </li>
-                <li> <Link  className="link" to="/Contact">Contact-Us</Link></li>
-               </ul>
-               </div>
-              <div className='avater-icon' onClick={() => setIsMobile(!isMobile)}>
-               {isMobile ? (<img src={ Avater} alt='avatar images' className='icon-img'/>) : (
-                <div className='close-button'>X</div>)}
-              </div>
-            </div>
-        </div>
+    <div className="navbar">
+        <div className="navbar-logo">
+           <img src={logo} alt='Company-logo' />
+           <h1 className='appgenius'>AETHERWAVE <span className='techs'>TECHNOLOGIES</span></h1>
+         </div>
+
+      <ul className={`navbar-menu ${isMobileMenuOpen ? 'active' : ''}`}>
+        <li className="navbar-item"><a href="/">Home</a></li>
+        <li className="navbar-item"><a href="/About">About-Us</a></li>
+        <li className="navbar-item"><a href="/Services">Services</a></li>
+        <li className="navbar-item"><a href="/Impact">Impact</a></li>
+        <li className="navbar-item"><a href="Contact">Contact-Us</a></li>
+      </ul>
+      <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
+        <span className={`bar ${isMobileMenuOpen ? 'active' : ''}`} />
+        <span className={`bar ${isMobileMenuOpen ? 'active' : ''}`} />
+        <span className={`bar ${isMobileMenuOpen ? 'active' : ''}`} />
+      </div>
     </div>
   )
 }
+
+
+
 
 export default Navbar
